b68d06c4c11f578533d09e023fc5a6485ac07c34
055055 <snb4301@gmail.com>
2021-01-07 22:12:32 +0900

    로깅을 위한 필터 등록

--- user-api/src/main/java/com/api/ApiApp.java
+++ user-api/src/main/java/com/api/ApiApp.java
@@ -2,7 +2,9 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //filter scan
 @SpringBootApplication
 public class ApiApp {
 
---
+++ user-api/src/main/java/com/api/common/filter/Filter.java
@@ -0,0 +1,148 @@
+package com.api.common.filter;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class Filter extends OncePerRequestFilter {
+    private static final ThreadLocal<Long> startTimeHolder = new ThreadLocal<>();
+    private static final List<MediaType> VISIBLE_TYPES = Arrays.asList(
+            MediaType.valueOf("text/*"),
+            MediaType.APPLICATION_FORM_URLENCODED,
+            MediaType.APPLICATION_JSON,
+            MediaType.APPLICATION_XML,
+            MediaType.valueOf("application/*+json"),
+            MediaType.valueOf("application/*+xml"),
+            MediaType.MULTIPART_FORM_DATA
+    );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (isAsyncDispatch(request)) {
+            filterChain.doFilter(request, response);
+        } else {
+            doFilterWrapped(wrapRequest(request), wrapResponse(response), filterChain);
+        }
+    }
+
+    protected void doFilterWrapped(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            beforeRequest(request, response);
+            filterChain.doFilter(request, response);
+        }
+        finally {
+            afterRequest(request, response);
+            response.copyBodyToResponse();
+        }
+    }
+
+    protected void beforeRequest(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        //set StartTime
+        startTimeHolder.set(System.nanoTime());
+        if (log.isInfoEnabled()) {
+            logRequestHeader(request, request.getRemoteAddr() + "|>");
+        }
+    }
+
+    protected void afterRequest(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        if (log.isInfoEnabled()) {
+            log.info("Request URI : {} ", request.getRequestURI());
+            logRequestBody(request, request.getRemoteAddr() + "|>");
+            logResponse(response, request.getRemoteAddr() + "|<");
+        }
+    }
+
+    private static void logRequestHeader(ContentCachingRequestWrapper request, String prefix) {
+        val queryString = request.getQueryString();
+        if (queryString == null) {
+            log.info("{} {} {}", prefix, request.getMethod(), request.getRequestURI());
+        } else {
+            log.info("{} {} {}?{}", prefix, request.getMethod(), request.getRequestURI(), queryString);
+        }
+        Collections.list(request.getHeaderNames()).forEach(headerName ->
+                Collections.list(request.getHeaders(headerName)).forEach(headerValue ->
+                        log.info("{} {}: {}", prefix, headerName, headerValue)));
+        log.info("{}", prefix);
+    }
+
+    private static void logRequestBody(ContentCachingRequestWrapper request, String prefix) {
+        val content = request.getContentAsByteArray();
+        if (content.length > 0) {
+            logContent(content, request.getContentType(), request.getCharacterEncoding(), prefix);
+        }
+    }
+
+    private static void logResponse(ContentCachingResponseWrapper response, String prefix) {
+        int status = response.getStatus();
+        log.info("{} {} {}", prefix, status, HttpStatus.valueOf(status).getReasonPhrase());
+
+        val content = response.getContentAsByteArray();
+        if (content.length > 0) {
+            logContent(content, response.getContentType(), response.getCharacterEncoding(), prefix);
+        }
+        //set EndTime
+
+        Long beforeNanoSec = startTimeHolder.get();
+        long elapsedNanoSec = System.nanoTime() - beforeNanoSec;
+        long elapseMilliSec = NANOSECONDS.toMillis(elapsedNanoSec);
+        startTimeHolder.remove();
+        log.info("Elapsed :{}ms", elapseMilliSec);
+
+    }
+
+    private static void logContent(byte[] content, String contentType, String contentEncoding, String prefix) {
+        val mediaType = MediaType.valueOf(contentType);
+        val visible = VISIBLE_TYPES.stream().anyMatch(visibleType -> visibleType.includes(mediaType));
+        if (visible) {
+            try {
+                val contentString = new String(content, contentEncoding);
+                Stream.of(contentString.split("\r\n|\r|\n")).forEach(line -> log.info("{} {}", prefix, line));
+            } catch (UnsupportedEncodingException e) {
+                log.info("{} [{} bytes content]", prefix, content.length);
+            }
+        } else {
+            log.info("{} [{} bytes content]", prefix, content.length);
+        }
+    }
+
+    private static ContentCachingRequestWrapper wrapRequest(HttpServletRequest request) {
+        if (request instanceof ContentCachingRequestWrapper) {
+            return (ContentCachingRequestWrapper) request;
+        } else {
+            return new ContentCachingRequestWrapper(request);
+        }
+    }
+
+    private static ContentCachingResponseWrapper wrapResponse(HttpServletResponse response) {
+        if (response instanceof ContentCachingResponseWrapper) {
+            return (ContentCachingResponseWrapper) response;
+        } else {
+            return new ContentCachingResponseWrapper(response);
+        }
+    }
+
+
+
+}
