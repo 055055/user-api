cdf48be7c98ef5da478b2b758c25364717a40368
055055 <snb4301@gmail.com>
2021-01-09 22:52:38 +0900

    이전 커밋 미반영분 추가

---
+++ user-api/src/main/java/com/api/web/dto/AccountUpdateResDto.java
@@ -0,0 +1,20 @@
+package com.api.web.dto;
+
+import com.api.entitiy.user.Account;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Setter
+public class AccountUpdateResDto {
+    private Long seq;
+    private String email;
+    private String name;
+    private Account.Role role;
+    private LocalDateTime createDate;
+    private LocalDateTime modifiedDate;
+}
